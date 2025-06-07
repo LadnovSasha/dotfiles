964935c7ab4b93b32e9fbcbd6ab7b84c27594ca2
Oleksandr Ladnov <sasha.ladnov@khealth.com>
2025-06-07 22:57:12 +0300

    Add nvim

---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,19 @@
+return {
+  { "rose-pine/neovim", name = "rose-pine" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      scroll = { enabled = false },
+      animate = { enabled = false },
+      indent = {
+        animate = { enabled = false },
+      },
+    },
+  },
+}
